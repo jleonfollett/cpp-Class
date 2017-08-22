8b12136b0eee021b6b27b739c64ba7d2565848ed
jleonfollett <jleonfollett@gmail.com>
2017-08-22 18:16:02 -0500

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "HelloWorld.h"
+using namespace std;
+
+void HelloWorld::sayHelloWorld() {
+	cout << "Hello John";
+	cin.ignore();
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "HelloWorld.h"
+using namespace std;
+
+int main() {
+	HelloWorld hw;
+	hw.sayHelloWorld();
+	return 0;
+}
+
